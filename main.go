53e4c9968ada53f9d910016f23d163968b5de62d
bytebit <huangjunfei@bupt.edu.cn>
2024-02-25 11:30:49 +0800

    0225

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package jtt1078
 import (
 	"net/http"
 
-	"go.uber.org/zap"
 	. "m7s.live/engine/v4"
 	"m7s.live/engine/v4/config"
 	"m7s.live/engine/v4/track"
@@ -12,42 +11,39 @@ import (
 /*
 自定义配置结构体
 配置文件中可以添加相关配置来设置结构体的值
-Jtt1078:
+JTT1078:
 
 	http:
 	publish:
 	subscribe:
 	foo: bar
 */
-type Jtt1078Config struct {
+type JTT1078Config struct {
 	config.HTTP
 	config.Publish
 	config.Subscribe
 	config.TCP
 }
 
-var conf = Jtt1078Config{
+var conf = &JTT1078Config{
 	TCP: config.TCP{ListenAddr: ":7222"},
 }
 
 // 安装插件
-var Jtt1078Plugin = InstallPlugin(&conf)
+var JTT1078Plugin = InstallPlugin(conf)
 
 // 插件事件回调，来自事件总线
-func (c *Jtt1078Config) OnEvent(event any) {
+func (c *JTT1078Config) OnEvent(event any) {
 	switch event.(type) {
-	case FirstConfig:
-		if c.TCP.ListenAddr != "" {
-			Jtt1078Plugin.Info("server jtt1078 start at", zap.String("listen addr", c.TCP.ListenAddr))
-			go c.ListenTCP(Jtt1078Plugin, c)
-		}
+	case FirstConfig: // 插件启动事件
+		break
 	}
 }
 
-// http://localhost:8080/Jtt1078/api/test/pub
-func (conf *Jtt1078Config) API_test_pub(rw http.ResponseWriter, r *http.Request) {
-	var pub Jtt1078Publisher
-	err := Jtt1078Plugin.Publish("jtt1078/test", &pub)
+// http://localhost:8080/JTT1078/api/test/pub
+func (conf *JTT1078Config) API_test_pub(rw http.ResponseWriter, r *http.Request) {
+	var pub JTT1078Publisher
+	err := JTT1078Plugin.Publish("JTT1078/test", &pub)
 	if err != nil {
 		rw.Write([]byte(err.Error()))
 		return
@@ -59,10 +55,10 @@ func (conf *Jtt1078Config) API_test_pub(rw http.ResponseWriter, r *http.Request)
 	rw.Write([]byte("test_pub"))
 }
 
-// http://localhost:8080/Jtt1078/api/test/sub
-func (conf *Jtt1078Config) API_test_sub(rw http.ResponseWriter, r *http.Request) {
-	var sub Jtt1078Subscriber
-	err := Jtt1078Plugin.Subscribe("jtt1078/test", &sub)
+// http://localhost:8080/JTT1078/api/test/sub
+func (conf *JTT1078Config) API_test_sub(rw http.ResponseWriter, r *http.Request) {
+	var sub JTT1078Subscriber
+	err := JTT1078Plugin.Subscribe("JTT1078/test", &sub)
 	if err != nil {
 		rw.Write([]byte(err.Error()))
 		return
@@ -73,27 +69,35 @@ func (conf *Jtt1078Config) API_test_sub(rw http.ResponseWriter, r *http.Request)
 }
 
 // 自定义发布者
-type Jtt1078Publisher struct {
+type JTT1078Publisher struct {
 	Publisher
 }
 
 // 发布者事件回调
-func (pub *Jtt1078Publisher) OnEvent(event any) {
-	switch event.(type) {
-	case IPublisher:
-		// 发布成功
+func (pub *JTT1078Publisher) OnEvent(event any) {
+	switch v := event.(type) {
+	case IPublisher: //代表发布成功事件
+	case SEclose: //代表关闭事件
+	case SEKick: //被踢出
+	case ISubscriber:
+		if v.IsClosed() {
+			//订阅者离开
+		} else {
+			//订阅者进入
+		}
+
 	default:
 		pub.Publisher.OnEvent(event)
 	}
 }
 
 // 自定义订阅者
-type Jtt1078Subscriber struct {
+type JTT1078Subscriber struct {
 	Subscriber
 }
 
 // 订阅者事件回调
-func (sub *Jtt1078Subscriber) OnEvent(event any) {
+func (sub *JTT1078Subscriber) OnEvent(event any) {
 	switch event.(type) {
 	case ISubscriber:
 		// 订阅成功
--- server.go
+++ server.go
@@ -1,27 +1,84 @@
 package jtt1078
 
 import (
+	"bufio"
 	"fmt"
 	"net"
 
 	"go.uber.org/zap"
+	. "m7s.live/engine/v4"
+	"m7s.live/engine/v4/util"
 )
 
-func (config *Jtt1078Config) ServeTCP(conn net.Conn) {
-	defer conn.Close()
-	logger := Jtt1078Plugin.Logger.With(zap.String("remote", conn.RemoteAddr().String()))
-	var err error
-	logger.Info("conn")
-	defer func() {
-		ze := zap.Error(err)
-		logger.Info("conn close", ze)
-	}()
-	nc := NewNetConnection(conn)
-	var pkg *Jtt1078RTP
-	for {
-		pkg, err = nc.RecvJtt1078RTP()
-		fmt.Print(pkg)
-		logger.Info("rtmp client closed")
-		return
+type NetStream struct {
+	*NetConnection
+}
+
+type AVSender struct {
+	*JTT1078Sender
+}
+
+type JTT1078Receiver struct {
+	Publisher
+}
+
+type JTT1078Sender struct {
+	Subscriber
+}
+
+type NetConnection struct {
+	*bufio.Reader `json:"-" yaml:"-"`
+	net.Conn      `json:"-" yaml:"-"`
+}
+
+func NewNetConnection(conn net.Conn) *NetConnection {
+	return &NetConnection{
+		Conn:   conn,
+		Reader: bufio.NewReader(conn),
 	}
+}
+
+// func (config *JTT1078Config) ServeTCP(conn net.Conn) {
+// 	defer conn.Close()
+// 	logger := JTT1078Plugin.Logger.With(zap.String("remote", conn.RemoteAddr().String()))
+// 	senders := make(map[uint32]*JTT1078Subscriber)
+// 	receivers := make(map[uint32]*JTT1078Receiver)
+// 	var err error
+// 	logger.Info("conn")
+// 	defer func() {
+// 		ze := zap.Error(err)
+// 		logger.Info("conn close", ze)
+// 		for _, sender := range senders {
+// 			sender.Stop(ze)
+// 		}
+// 		for _, receiver := range receivers {
+// 			receiver.Stop(ze)
+// 		}
+// 	}()
+// 	nc := NewNetConnection(conn)
+// 	for {
+// 		if msg, err = nc.RecvMessage(); err == nil {
+// 			if msg.MessageLength <= 0 {
+// 				continue
+// 			}
+// 			switch msg.MessageTypeID {
+// 			case RTMP_MSG_AMF0_COMMAND:
+// 			}
+// 		}
+// 	}
+// }
+
+func (c *JTT1078Config) ServeTCP(conn net.Conn) {
+	fmt.Println("JTT1078 ServeTCP...")
+	reader := TCP1078RTP{
+		Conn: conn,
+	}
+
+	reader.Start(func(data util.Buffer) (err error) {
+		var rtpPacket Packet
+		if err = rtpPacket.Unmarshal(data); err != nil {
+			JTT1078Plugin.Error("JTT1078 decode rtp error:", zap.Error(err))
+		}
+		return
+	})
 }
